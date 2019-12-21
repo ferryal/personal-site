b4774ab4101bfddefaaf49439d91aa31b16f6155
ferryal <ferryal.bahari@amartha.com>
2019-12-21 19:15:33 +0700

    add app jsr

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import LandingPage from "./pages/LandingPage";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html {
+      font-family: Roboto, "Helvetica Neue", Helvetica, Arial, Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0; 
+    }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <LandingPage></LandingPage>
+    </>
+  );
+}
+
+export default App;
